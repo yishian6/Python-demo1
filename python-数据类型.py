84a6f5db0e6ebca7229ea6ef7314f3004723d607
yishian <2285449638@qq.com>
2022-08-22 14:12:53 +0800

    second commit

--- python-数据类型.py
+++ python-数据类型.py
@@ -10,3 +10,14 @@ Int_type = type(666)
 float_type = type(1.3)
 
 print(String_type, Int_type, float_type)
+
+
+# 数据格式转换
+
+# 将整数转换成字符串
+str_type = str(666)
+print(type(str_type))
+
+# 将浮点数转换成字符串
+str_type1 = str(1.3331)
+print(type(str_type1), str_type)
